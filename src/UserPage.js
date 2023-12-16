7cdb6593bef11d9923f291ca7ec031218d220503
Shankar <1235813.ss@gmail.com>
2023-12-16 11:35:04 +0530

    changes in urls

--- src/UserPage.js
+++ src/UserPage.js
@@ -2,18 +2,21 @@ import React from "react";
 
 const UserPage = () => {
   return (
-    <div className="App">
-      <h1>
-        Hello <span className="name">{localStorage.getItem("userName")}</span>
-      </h1>
-      <div>
-        <h2>Your Name is : {localStorage.getItem("userName")}</h2>
-        <h2>Your Email is : {localStorage.getItem("userEmail")}</h2>
-        <h2>Your Mobile No. is : {localStorage.getItem("userMobile")}</h2>
+    <div className="hh">
+      <h1 className="user">USER AUTHENTICATION</h1>
+      <div className="App">
+        <h1>
+          Hello <span className="name">{localStorage.getItem("userName")}</span>
+        </h1>
+        <div>
+          <h2>Your Name is : {localStorage.getItem("userName")}</h2>
+          <h2>Your Email is : {localStorage.getItem("userEmail")}</h2>
+          <h2>Your Mobile No. is : {localStorage.getItem("userMobile")}</h2>
+        </div>
+        <button className="logout" onClick={logout}>
+          Logout
+        </button>
       </div>
-      <button className="logout" onClick={logout}>
-        Logout
-      </button>
     </div>
   );
 };
